7924b149726ff9a3412588930e32c60ef18c7e8f
Juliana Lucena <juliana.lucena@incognia.com>
2023-06-12 14:51:54 -0300

    Increase process_timeout for browser tests

--- spec/system/support/cuprite.rb
+++ spec/system/support/cuprite.rb
@@ -9,7 +9,7 @@ Capybara.register_driver(:better_cuprite) do |app|
     window_size: [1200, 800],
     #browser_options: { 'no-sandbox': nil },
     # Increase Chrome startup wait time (required for stable CI builds)
-    process_timeout: 10,
+    process_timeout: 20,
     # Enable debugging capabilities
     inspector: true,
     # Allow running Chrome in a headful mode by setting HEADLESS env
